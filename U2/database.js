3632c61512b0d9596809b6c415b187cc8e18b032
MadeByGabbe <97948127+MadeByGabbe@users.noreply.github.com>
2022-03-01 15:42:30 +0100

    First Base
    - Database corrections and error fixes
    
    - Html rewriting with correct ids
    
    - Main.js: Wrote over code from lectures with database adaptation and slight adjustments to fit the theme and to get a base code to add upon
    
    - Styles also copied to easier see if it all works (will be further developed)

--- U2/database.js
+++ U2/database.js
@@ -1,74 +1,75 @@
 // Our database of dogs
+// Our database of cars
 let database = [
     {
-        id = "1",
+        id: 1,
         model: "XC90",
         body: "SUV",
-        price: "725000",
+        price: 725000,
         drivetrain: "AWD"
 
     },
     {
-        id = "2",
+        id: 2,
         model: "XC60",
         body: "SUV",
-        price: "468000",
+        price: 468000,
         drivetrain: "FWD"
     },
     {
-        id = "3",
+        id: 3,
         model: "XC40",
         body: "SUV",
-        price: "298000",
+        price: 298000,
         drivetrain: "FWD"
     },
     {
-        id = "4",
+        id: 4,
         model: "S90",
         body: "Sedan",
-        price: "461000",
+        price: 461000,
         drivetrain: "FWD"
     },
     {
-        id = "5",
+        id: 5,
         model: "S60",
         body: "Sedan",
-        price: "407200",
+        price: 407200,
         drivetrain: "FWD"
     },
     {
-        id = "6",
+        id: 6,
         model: "V90",
         body: "Wagon",
-        price: "468000",
+        price: 468000,
         drivetrain: "FWD"
     },
     {
-        id = "7",
+        id: 7,
         model: "V60",
         body: "Wagon",
-        price: "404000",
+        price: 404000,
         drivetrain: "FWD"
     },
     {
-        id = "8",
+        id: 8,
         model: "V90 Cross Country",
         body: "Wagon",
-        price: "500000",
+        price: 500000,
         drivetrain: "AWD"
     },
     {
-        id = "9",
+        id: 9,
         model: "V60 Cross Country",
         body: "Wagon",
-        price: "454000",
+        price: 454000,
         drivetrain: "AWD"
     },
     {
-        id = "10",
+        id: 10,
         model: "C40 Recharge",
         body: "Crossover",
-        price: "539900",
+        price: 539900,
         drivetrain: "FWD"
     }
 ];
--- U2/main.js
+++ U2/main.js
@@ -12,4 +12,138 @@ function createNewCar(model, body, price, drivetrain) {
     return car
 }
 
+function addCarToDatabase(database, car) {
+    database.push(car);
+}
 
+function removeCarById(cars, id) {
+    for (let i = 0; i < cars.length; i++) {
+        let car = cars[i];
+        if (car.id == id) {
+            cars.splice(i, 1);
+            return;
+        }
+    }
+}
+
+function getCarsByBody(cars, body) {
+    let carsByBody = [];
+
+    for (let car of cars) {
+        if (car.body.toLowerCase() == body.toLowerCase()) {
+            carsByBody.push(car);
+        } 
+    }
+
+    return carsByBody;
+}
+
+function getCarsByDrivetrain(cars, drivetrain) {
+    let carsByDrivetrain = [];
+
+    for (let car of cars) {
+        if (car.drivetrain == drivetrain) {
+            carsByDrivetrain.push(car)
+        }
+    }
+    
+}
+
+function injectCar(car) {
+    let div = document.createElement("div");
+    div.classList.add("car");
+    div.id = car.id
+
+    div.innerHTML = `
+    <div>${car.model}</div>
+    <div>${car.body}</div>
+    <div>${car.price}</div>
+    <div>${car.drivetrain}</div>
+    <button type="button">Remove</button>
+    `;
+
+    return div;
+}
+
+function injectCars(cars) {
+    let carsElement = document.getElementById("cars");
+    carsElement.innerHTML = "";
+
+    for (let car of cars) {
+        let carElement = injectCar(car);
+        carsElement.appendChild(carElement);
+    }
+
+    setRemoveDogHandlers();
+}
+
+function onAddCarSubmit(event) {
+    event.preventDefault();
+
+    let model = document.getElementById("model").value;
+    let body = document.getElementById("body").value;
+    let price = Number(document.getElementById("price").value);
+    let drivetrain = document.getElementById("drivetrain").value;
+
+
+
+    let car = createNewCar(model, body, price, drivetrain);
+    addCarToDatabaseFromPrompt(database, car);
+    renderCars(database);
+
+    let form = document.getElementById("add-car-form");
+    form.reset();
+}
+
+function setAddCarHandler() {
+    let form = document.getElementById("add-car-form");
+    form.addEventListener("submit", onAddCarSubmit);
+}
+
+function onRemoveCarClick(event) {
+    let button = event.target;
+    let id = button.parentElement.id;
+    removeCarById(database, id);
+    renderCars(cars);
+}
+
+function setRemoveCarHandlers() {
+    let buttons = document.querySelectorAll(".car button");
+
+    for (let button of buttons) {
+        button.addEventListener("click", onRemoveCarClick);
+    }
+}
+
+function onFilterByBody(event) {
+    event.preventDefault();
+    let body = document.getElementById("filter-body").value;
+    let cars = getCarsByBody(database, body);
+    injectCars(cars);
+}
+
+function onFilterByDrivetrain(event) {
+    event.preventDefault();
+    let drivetrain = document.getElementById("filter-drivetrain").value;
+    let cars = getCarsByDrivetrain(database, drivetrain)
+}
+
+function onListAllClick() {
+    document.getElementById("filter-body").value = "";
+    document.getElementById("filter-drivetrain").value = "";
+    injectCars(database);
+}
+
+function setFilterCarHandlers() {
+    let body_form = document.getElementById("filter-by-body");
+    let drivetrain_form = document.getElementById("filter-by-drivetrain")
+    let listAll = document.getElementById("list-all");
+
+    body_form.addEventListener("submit", onFilterByBody);
+    drivetrain_form.addEventListener("submit", onFilterByDrivetrain);
+    listAll.addEventListener("click", onListAllClick);
+}
+
+injectCars(database);
+setAddCarHandler();
+setFilterCarHandlers();
